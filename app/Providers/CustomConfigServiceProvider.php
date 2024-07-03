3f5aa93b3da68726cde419ca8f80b2c828ec03c4
pride <pride@propertybook.co.zw>
2024-07-03 12:38:23 +0200

    gg

--- app/Providers/CustomConfigServiceProvider.php
+++ app/Providers/CustomConfigServiceProvider.php
@@ -20,7 +20,7 @@ class CustomConfigServiceProvider extends ServiceProvider
         if (Schema::hasTable('settings')) {
             $smtp_settings = DB::table('settings')->where('type', 'smtp')->pluck('description', 'label');
             if (!$smtp_settings->isEmpty()) {
-                json_decode($smtp_settings['from']); 
+                json_decode($smtp_settings['from']);
                 $smtp_config = [
                     'transport' => 'smtp',
                     'host' => $smtp_settings['smtp_host'],
@@ -42,26 +42,28 @@ class CustomConfigServiceProvider extends ServiceProvider
                     'branch' => $email_settings['branch'] ?? '',
                     'acc_number' => $email_settings['acc_number'] ?? '',
                     'acc_name' => $email_settings['acc_name'] ?? '',
-                    'show_bank' => $email_settings['show_bank'] ?? ''
+
+                    'nostro_1_bank' => $email_settings['nostro_1_bank'] ?? '',
+                    'nostro_1_branch' => $email_settings['nostro_1_branch'] ?? '',
+                    'nostro_1_acc_number' => $email_settings['nostro_1_acc_number'] ?? '',
+                    'nostro_1_acc_name' => $email_settings['nostro_1_acc_name'] ?? '',
+
+                    'nostro_2_bank' => $email_settings['nostro_2_bank'] ?? '',
+                    'nostro_2_branch' => $email_settings['nostro_2_branch'] ?? '',
+                    'nostro_2_acc_number' => $email_settings['nostro_2_acc_number'] ?? '',
+                    'nostro_2_acc_name' => $email_settings['nostro_2_acc_name'] ?? '',
+
+                    'ecco_number' => $email_settings['ecco_number'] ?? '',
+                    'ecco_name' => $email_settings['ecco_name'] ?? '',
+
+                    'show_eco' => $email_settings['show_eco'] ?? '',
+                    'show_bank' => $email_settings['show_bank'] ?? '',
+                    'show_nostro_1' => $email_settings['show_nostro_1'] ?? '',
+                    'show_nostro_2' => $email_settings['show_nostro_2'] ?? '',
 
                 ];
                 config(['config.EMAIL' => $email_config]);
             }
-
-            $imap_settings = DB::table('settings')->where('type', 'imap')->pluck('description', 'label');
-            if (!$imap_settings->isEmpty()) {
-                $imap_config = [
-                    'ImapHost' => $imap_settings['imap_host'] ?? env('IMAP_HOST'),
-                    'ImapPort' => $imap_settings['imap_port'] ?? env('IMAP_PORT'),
-                    'ImapUser' => $imap_settings['imap_user'] ?? env('IMAP_USER'),
-                    'ImapPass' => $imap_settings['imap_pass'] ?? env('IMAP_PASS'),
-                    'ImapSentFolder' => $imap_settings['imap_sent_folder'] ?? env('IMAP_SENTFOLDER'),
-                    'ImapEncryption' => $imap_settings['imap_encryption'] ?? env('IMAP_ENCRYPTION'),
-                    
-                ];
-                config(['mail.imap' => $imap_config]);
-            }
-
 
         }
 
